0c5c186e9108795ccb840d96103bfc013a93fc1a
Connor Belman <connorbelman@gmail.com>
2019-12-27 02:06:09 -0500

    fixed javascipr for Safari browser

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function SetPalette() {
 		document.getElementById('headerImage').src = 'images/header_main/header2.png';
 		document.getElementById('header').style.backgroundImage = 'url("images/header_bg/headerbg2.png")';
 		document.body.style.backgroundColor = '#111227';
-		document.getElementById('info').style.color = '#272652';
+		document.getElementById('title').style.color = '#272652';
+		document.getElementById('email').style.color = '#272652';
 	}
 	// Sunrise
 	else if (n >= 6 && n <= 8) {
@@ -16,7 +17,8 @@ function SetPalette() {
 		document.getElementById('headerImage').src = 'images/header_main/header3.png';
 		document.getElementById('header').style.backgroundImage = "url('images/header_bg/headerbg3.png')";
 		document.body.style.backgroundColor = '#f66666';
-		document.getElementById('info').style.color = '#f29a7f';
+		document.getElementById('title').style.color = '#f29a7f';
+		document.getElementById('email').style.color = '#f29a7f';
 	}
 	// Day
 	else if (n >= 9 && n <= 18) {
@@ -24,6 +26,7 @@ function SetPalette() {
 		document.getElementById('headerImage').src = 'images/header_main/header4.png';
 		document.getElementById('header').style.backgroundImage = "url('images/header_bg/headerbg4.png')";
 		document.body.style.backgroundColor = '#8bc34a';
-		document.getElementById('info').style.color = '#43a047';
+		document.getElementById('title').style.color = '#43a047';
+		document.getElementById('email').style.color = '#43a047';
 	}
 }
